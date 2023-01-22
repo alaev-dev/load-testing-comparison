12cf2dba20f4f6f9ce5e4431854d7161bd1dcb6d
RightWing <alex.iskorkin@gmail.com>
2023-01-22 18:25:22 +0300

    0008 HOTFIX :: fix share implementation :: all

--- webflux/build.gradle.kts
+++ webflux/build.gradle.kts
@@ -16,7 +16,6 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":share"))
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
